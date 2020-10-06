3d678aee93bf0c8a6c665fb5fb45228b22edd945
fgerling <felix@fgerling.de>
2020-10-06 13:51:38 +0200

    Fixed variable substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 CONCOURSE_REPO="space-nerds-in-space-repo"
 
 function build_snis {
-	if [ ! -d "CONCOURSE_REPO" ]
+	if [ ! -d "${CONCOURSE_REPO}" ]
 	then
 		>&2 echo "[ERROR] No ${CONCOURSE_REPO} repo found"
 		    echo "[DEBUG] pwd: $(pwd)"
