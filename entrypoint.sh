4d33b9037fef2169095271140347a0351d808d69
fgerling <felix@fgerling.de>
2020-10-06 13:27:26 +0200

    Fix syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ CONCOURSE_REPO="space-nerds-in-space-repo"
 
 function build_snis {
 	if [ ! -d "CONCOURSE_REPO" ]
+	then
 		>&2 echo "[ERROR] No ${CONCOURSE_REPO} repo found"
 		    echo "[DEBUG] pwd: $(pwd)"
 			exit 1
